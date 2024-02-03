5f3d746bc1cdf3d3bb7bf16ae266be1d0468d6c5
Pablo Jelvez <pjelvezj@gmail.com>
2024-02-03 01:14:55 -0300

    Implementacion del buscador, del Header, Footer, aplicación de estilos y uso del metodo sort

--- src/components/MiApi/index.jsx
+++ src/components/MiApi/index.jsx
@@ -1,77 +1,54 @@
-import { useState, useEffect } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
 
-const MiApi = () =>{
-const [users, setUsers] = useState([])
+const MiApi = ({users}) =>{
+
+  return(
+      <>
+      <div className='divMiApi'> 
+      <h2>Nuestros productos</h2>
+
+      <div className='row row-cols-lg-4 row-cols-md-2 row-cols-sm-1'>
+      {users.map((user) => (
+      <Card style={{ width: '18rem' }}>
+      <Card.Img className='p-4' variant="top" src={user.image} />
+      <Card.Body>
+        <Card.Title>{user.title}</Card.Title>
+        <Card.Text>{user.description}</Card.Text>
+        <p className='priceCard'>Precio: <span className='spanCard'>{user.price}</span></p>
+      </Card.Body>
+      </Card>
+      ))}
+      </div>
+      </div>
  
-    
- const getData =  () => {
-    const route = "https://fakestoreapi.com/products"
-
-   fetch(route)
-   .then((response) => response.json())
-   .then((formatedResponse) => setUsers(formatedResponse))
-   .catch((error) => console.log(error))
-    
- };
-
- useEffect(() => {
-    if (users.length === 0) {
-      getData();
-    }
-  }, []); 
-
-  useEffect(() => {
-    console.log(users);
-  }, [users]);
-
-
-    return(
-        <>
-  
-        <div>
-           <table className="table table-striped table-hover my-4 shadow-lg">
-            <thead>
-                <tr>
-                    <th>Producto</th>
-                    <th>Precio</th>
-                    <th>Descripcion</th>
-                    <th>Foto</th>
-                </tr>
-            </thead>
-            <tbody>
-                {users.map((user) => (
-                    <tr key={user.id}>
-                        <td>{user.title}</td>
-                        <td>{user.price}</td>
-                        <td>{user.description}</td>
-                        <td><img src={user.image} alt="" width={100} /></td>
-                    </tr>
-                ))}
-
-            </tbody>
-        </table> 
-        </div>
-        
-
-        </>
       
-    );
+
+      </>
+    
+  );
 };
 
 export default MiApi;
 
-//      <h1>Listado de usuarios</h1>
-//        <ul>
-//            {
-//            users.map((user) => (
-//            <li key={user.id.value}>
-//                <h3>Nombre: {`${user.name.first} ${user.name.last}`}</h3>
-//                 <p>Email: {user.email}</p>
-//                 <p>Telefono: {user.phone}</p>
-//                 <img src={user.picture.large} alt="" />
-//            </li>
-//         
-//        
-//        ))}
-//        </ul>
+
+//<table className="table table-striped table-hover my-4 shadow-lg">
+//<thead>
+//    <tr>
+//        <th>Producto</th>
+//        <th>Precio</th>
+//        <th>Descripcion</th>
+//        <th>Foto</th>
+//    </tr>
+//</thead>
+//<tbody>
+//    {users.map((user) => (
+//        <tr key={user.id}>
+//            <td>{user.title}</td>
+//            <td>{user.price}</td>
+//            <td>{user.description}</td>
+//            <td><img src={user.image} alt="" width={100} /></td>
+//        </tr>
+//    ))}
+//
+//</tbody>
+//</table> 
